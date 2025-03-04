67057aba4516110f36cc6045be3d2ba448136925
Juan Martín Sánchez <jmartin_sanchez@javeriana.edu.co>
2025-03-03 22:47:03 -0500

    MySQL conectado funcionando

--- tufinca/src/main/java/co/edu/javeriana/tufinca/entities/Usuario.java
+++ tufinca/src/main/java/co/edu/javeriana/tufinca/entities/Usuario.java
@@ -1,5 +1,29 @@
 package co.edu.javeriana.tufinca.entities;
 
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Where(clause = "status = 0")
+@SQLDelete(sql = "UPDATE application SET status = 1 WHERE id=?")
 public class Usuario {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String apellido;
 }
