a82a756995ff8b09e5c1bf2c531d6566cb091e47
Palmxl <jmartin_sanchez@javeriana.edu.co>
2025-04-25 23:15:51 -0500

    Correccion JWT

--- tufinca/src/main/java/co/edu/javeriana/tufinca/security/service/MyUserDetailsService.java
+++ tufinca/src/main/java/co/edu/javeriana/tufinca/security/service/MyUserDetailsService.java
@@ -6,13 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
 import org.springframework.security.core.userdetails.User;
 
 import co.edu.javeriana.tufinca.entities.Usuario;
 import co.edu.javeriana.tufinca.repositories.UsuarioRepository;
 
-public class MyUserDetailsService {
+public class MyUserDetailsService implements UserDetailsService {
 
     @Autowired
     private UsuarioRepository usuarioRepository;
