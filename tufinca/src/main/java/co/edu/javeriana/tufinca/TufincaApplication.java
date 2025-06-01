4e6c10bbb4da940a13f220b457756a9a58a1012c
Palmxl <jmartin_sanchez@javeriana.edu.co>
2025-06-01 04:41:34 -0500

    Correccion hibernate

--- tufinca/src/main/java/co/edu/javeriana/tufinca/TufincaApplication.java
+++ tufinca/src/main/java/co/edu/javeriana/tufinca/TufincaApplication.java
@@ -2,8 +2,14 @@ package co.edu.javeriana.tufinca;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "co.edu.javeriana.tufinca.repositories")
+@ComponentScan(basePackages = "co.edu.javeriana.tufinca")
+@EntityScan(basePackages = "co.edu.javeriana.tufinca.entities")
 public class TufincaApplication {
 
 	public static void main(String[] args) {
