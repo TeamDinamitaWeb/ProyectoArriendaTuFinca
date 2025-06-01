ce094270e00376f29ada7ba251a7455d56261e2f
Palmxl <jmartin_sanchez@javeriana.edu.co>
2025-05-31 20:38:26 -0500

    Enum TipoUsuario

--- tufinca/src/main/java/co/edu/javeriana/tufinca/entities/Usuario.java
+++ tufinca/src/main/java/co/edu/javeriana/tufinca/entities/Usuario.java
@@ -35,8 +35,12 @@ public class Usuario implements Serializable {
     private String apellido;
     private String correo;
     private String contrasena;
-    private String tipoUsuario;
 
     @Column(nullable = false)
     private Integer status = 0;
+
+    public enum TipoUsuario {
+        ARRENDADOR,
+        ARRENDATARIO
+    }
 }
