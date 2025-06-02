4eec55922f1dd8b2f6fb2ea2a51ab87a91877ccc
Palmxl <jmartin_sanchez@javeriana.edu.co>
2025-06-01 19:55:07 -0500

    Cross origins UsuarioController

--- tufinca/src/main/java/co/edu/javeriana/tufinca/controllers/UsuarioController.java
+++ tufinca/src/main/java/co/edu/javeriana/tufinca/controllers/UsuarioController.java
@@ -15,28 +15,31 @@ import co.edu.javeriana.tufinca.services.UsuarioService;
 
 @RestController
 @RequestMapping("/api/usuarios")
-@CrossOrigin(origins = "http://localhost:4200")
 public class UsuarioController {
 
     @Autowired
     private UsuarioService usuarioService;
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping
     public ResponseEntity<List<UsuarioDTOs>> obtenerTodos() {
         return ResponseEntity.ok(usuarioService.obtenerTodos());
     }
     
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/all-including-deleted")
     public ResponseEntity<List<UsuarioDTOs>> obtenerTodosInclusoEliminados() {
         return ResponseEntity.ok(usuarioService.obtenerTodosInclusoEliminados());
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/{id}")
     public ResponseEntity<UsuarioDTOs> obtenerPorId(@PathVariable Long id) {
         UsuarioDTOs usuario = usuarioService.obtenerPorId(id);
         return usuario != null ? ResponseEntity.ok(usuario) : ResponseEntity.notFound().build();
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @PostMapping("/crear")
     public ResponseEntity<?> crearUsuario(@RequestBody Usuario usuario) {
         try {
@@ -47,23 +50,27 @@ public class UsuarioController {
         }
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @PutMapping("/{id}")
     public ResponseEntity<UsuarioDTOs> actualizarUsuario(@PathVariable Long id, @RequestBody Usuario usuario) {
         UsuarioDTOs usuarioActualizado = usuarioService.actualizarUsuario(id, usuario);
         return usuarioActualizado != null ? ResponseEntity.ok(usuarioActualizado) : ResponseEntity.notFound().build();
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> eliminarUsuario(@PathVariable Long id) {
         return usuarioService.eliminarUsuario(id) ? ResponseEntity.noContent().build() : ResponseEntity.notFound().build();
     }
 
     // JWT
+    @CrossOrigin(origins = "http://localhost:4200")
     @PostMapping(value = "/jwt-decode", produces = MediaType.APPLICATION_JSON_VALUE)
     public UsuarioDTOs usuarioDesdeToken(Authentication authentication) throws Exception {
         return usuarioService.autorizacion(authentication);
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping(value = "/error", produces = MediaType.APPLICATION_JSON_VALUE)
     public UsuarioDTOs error(Authentication authentication) throws Exception {
         return usuarioService.autorizacion(authentication);
