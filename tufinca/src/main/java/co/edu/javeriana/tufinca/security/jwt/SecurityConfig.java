7d25453ec66df09b652c6ed3cc6b11fb3cc50a01
Palmxl <jmartin_sanchez@javeriana.edu.co>
2025-06-02 01:34:09 -0500

    SeciurityConfig, problema 403

--- tufinca/src/main/java/co/edu/javeriana/tufinca/security/jwt/SecurityConfig.java
+++ tufinca/src/main/java/co/edu/javeriana/tufinca/security/jwt/SecurityConfig.java
@@ -3,18 +3,17 @@ package co.edu.javeriana.tufinca.security.jwt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+//import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.security.web.util.matcher.OrRequestMatcher;
-import org.springframework.security.web.util.matcher.RequestMatcher;
+//import org.springframework.security.web.util.matcher.*;
 
 import co.edu.javeriana.tufinca.security.jwt.filter.JWTAuthorizationFilter;
 
@@ -47,7 +46,9 @@ public class SecurityConfig implements ISecurityConfig {
                 .requestMatchers("/api/usuarios/**").permitAll() // registro sin token
                 .anyRequest().authenticated()
             )
-            .addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class);
+            .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http.addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
